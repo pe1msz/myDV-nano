0ae823908d2eedd43d179ed0751a4513cca60e81
pe1msz <39623528+pe1msz@users.noreply.github.com>
2018-06-02 15:39:01 +0200

    Adding webinterface for Nano

---
+++ dvmega/flash_mega.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+systemctl stop pistar-watchdog.service
+systemctl stop dstarrepeater.service
+systemctl stop pistar-watchdog.timer
+systemctl stop dstarrepeater.timer
+systemctl stop mmdvmhost.timer
+systemctl stop mmdvmhost.service
+
+#sudo mount -o remount,rw /
+FIRMWARE=./dvmega/*.hex
+for found in $FIRMWARE
+do
+  echo "Found $found firmware..."
+  # take action on this file, upload it using special commands to dvmega.
+  /usr/bin/cast-avrdude -p m328p -c arduino -P /dev/ttyS2 -b 115200 -F -U flash:w:${found} -v 
+ 
+  # move to backup-folder, and reboot the unit
+  mv ${found} ./dvmega/backup
+  sudo reboot
+done

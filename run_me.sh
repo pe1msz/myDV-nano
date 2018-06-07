1962e4c2202d0728b0aca1c7f108bbbfdb439901
Ruud Kerstens <ruud@pe1msz.nl>
2018-06-07 19:50:09 +0200

    Update run_me.sh
    fixed a few errors.

--- run_me.sh
+++ run_me.sh
@@ -1,15 +1,15 @@
 sudo -i
 rpi-rw
 cd /usr/bin
-git clone https://github.com/castbin 
-cp ./castbin/* .
+git clone https://github.com/pe1msz/castbin 
+mv ./castbin/* .
 chmod +x cast-avrdude
 chmod +x cast-reset
 chmod +x cast-fan
 chmod +x cast-enable.sh
 cd /var/www/dashboard/admin
 git clone https://github.com/pe1msz/myDV-nano 
-cp ./myDV-nano/* .
+mv ./myDV-nano/* .
 chown -R www-data:www-data ./fw
 cd ./fw/pyserial-3.2.1
 sudo python ./setup.py install

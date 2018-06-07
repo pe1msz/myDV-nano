d1f8ca168ba24e319f89de3927c8f1efc6115d85
Ruud Kerstens <ruud@pe1msz.nl>
2018-06-07 19:54:38 +0200

    Update run_me.sh
    added a few cleanups in the script.

--- run_me.sh
+++ run_me.sh
@@ -3,6 +3,7 @@ rpi-rw
 cd /usr/bin
 git clone https://github.com/pe1msz/castbin 
 mv ./castbin/* .
+rm -r ./castbin
 chmod +x cast-avrdude
 chmod +x cast-reset
 chmod +x cast-fan
@@ -10,6 +11,7 @@ chmod +x cast-enable.sh
 cd /var/www/dashboard/admin
 git clone https://github.com/pe1msz/myDV-nano 
 mv ./myDV-nano/* .
+rm -r ./myDV-nano
 chown -R www-data:www-data ./fw
 cd ./fw/pyserial-3.2.1
 sudo python ./setup.py install

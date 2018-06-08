ea1d5b38f2f340ff6eb866880717afbd98e3a969
Ruud Kerstens <ruud@pe1msz.nl>
2018-06-08 11:56:23 +0200

    Update run_me.sh
    fix folder issue

--- run_me.sh
+++ run_me.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 sudo rpi-rw
-sudo cd /usr/bin
-sudo git clone https://github.com/pe1msz/castbin 
-sudo mv ./castbin/* .
-sudo rm -r ./castbin
-sudo chmod +x cast-avrdude
-sudo chmod +x cast-reset
-sudo chmod +x cast-fan
-sudo chmod +x cast-enable.sh
-sudo cd /var/www/dashboard/admin
-sudo git clone https://github.com/pe1msz/myDV-nano 
-sudo mv ./myDV-nano/* .
-sudo rm -r ./myDV-nano
-sudo chown -R www-data:www-data ./fw
-sudo cd ./fw/pyserial-3.2.1
-sudo python ./setup.py install
-sudo cd ..
-sudo cd ./dvmega
-sudo chmod +x flash_mega.sh
-
+sudo git clone https://github.com/pe1msz/castbin /usr/bin/castbin
+sudo mv /usr/bin/castbin/* /usr/bin/. 
+sudo rm -r /usr/bin/castbin
+sudo chmod +x /usr/bin/cast-avrdude
+sudo chmod +x /usr/bin/cast-reset
+sudo chmod +x /usr/bin/cast-fan
+sudo chmod +x /usr/bin/cast-enable.sh
+sudo git clone https://github.com/pe1msz/myDV-nano /var/www/dashboard/admin/myDV-nano
+sudo mv /var/www/dashboard/admin/myDV-nano/*  /var/www/dashboard/admin/.
+sudo rm -r /var/www/dashboard/admin/myDV-nano
+sudo chown -R www-data:www-data /var/www/dashboard/admin/fw
+sudo python /var/www/dashboard/admin/fw/pyserial-3.2.1/setup.py install
+sudo sudo chmod +x /var/www/dashboard/admin/fw/dvmega/flash_mega.sh

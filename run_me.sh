7ce1d7beb4cd38a9c090562e5ec29b5004cc27cc
Ruud Kerstens <ruud@pe1msz.nl>
2018-06-08 11:45:07 +0200

    Update run_me.sh

--- run_me.sh
+++ run_me.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
-sudo -i
-rpi-rw
-cd /usr/bin
-git clone https://github.com/pe1msz/castbin 
-mv ./castbin/* .
-rm -r ./castbin
-chmod +x cast-avrdude
-chmod +x cast-reset
-chmod +x cast-fan
-chmod +x cast-enable.sh
-cd /var/www/dashboard/admin
-git clone https://github.com/pe1msz/myDV-nano 
-mv ./myDV-nano/* .
-rm -r ./myDV-nano
-chown -R www-data:www-data ./fw
-cd ./fw/pyserial-3.2.1
+sudo rpi-rw
+sudo cd /usr/bin
+sudo git clone https://github.com/pe1msz/castbin 
+sudo mv ./castbin/* .
+sudo rm -r ./castbin
+sudo chmod +x cast-avrdude
+sudo chmod +x cast-reset
+sudo chmod +x cast-fan
+sudo chmod +x cast-enable.sh
+sudo cd /var/www/dashboard/admin
+sudo git clone https://github.com/pe1msz/myDV-nano 
+sudo mv ./myDV-nano/* .
+sudo rm -r ./myDV-nano
+sudo chown -R www-data:www-data ./fw
+sudo cd ./fw/pyserial-3.2.1
 sudo python ./setup.py install
-cd ..
-cd ./dvmega
-chmod +x flash_mega.sh
+sudo cd ..
+sudo cd ./dvmega
+sudo chmod +x flash_mega.sh
 

e475b9314a0f4d8ba677fcb8970d565638db4fda
Ruud Kerstens <ruud@pe1msz.nl>
2018-06-08 12:55:32 +0200

    Update run_me.sh
    fix for pyserial-setup

--- run_me.sh
+++ run_me.sh
@@ -8,8 +8,9 @@ sudo chmod +x /usr/bin/cast-reset
 sudo chmod +x /usr/bin/cast-fan
 sudo chmod +x /usr/bin/cast-enable.sh
 sudo git clone https://github.com/pe1msz/myDV-nano /var/www/dashboard/admin/myDV-nano
-sudo mv /var/www/dashboard/admin/myDV-nano/*  /var/www/dashboard/admin/.
+sudo mv /var/www/dashboard/admin/myDV-nano/*  /var/www/dashboard/admin/
 sudo rm -r /var/www/dashboard/admin/myDV-nano
 sudo chown -R www-data:www-data /var/www/dashboard/admin/fw
-sudo python /var/www/dashboard/admin/fw/pyserial-3.2.1/setup.py install
 sudo sudo chmod +x /var/www/dashboard/admin/fw/dvmega/flash_mega.sh
+cd /var/www/dashboard/admin/fw/pyserial-3.2.1 
+sudo python setup.py install

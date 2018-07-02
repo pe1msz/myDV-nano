c7ff2bb39bf714f564569a9c0a84ee9677a5c7f3
pe1msz <39623528+pe1msz@users.noreply.github.com>
2018-07-02 14:14:23 +0200

    removed file

--- run_me.sh
+++ run_me.sh
@@ -6,7 +6,6 @@ sudo rm -r /usr/bin/castbin
 sudo chmod +x /usr/bin/cast-avrdude
 sudo chmod +x /usr/bin/cast-reset
 sudo chmod +x /usr/bin/cast-fan
-sudo chmod +x /usr/bin/cast-enable.sh
 sudo git clone https://github.com/pe1msz/myDV-nano /var/www/dashboard/admin/myDV-nano
 sudo mv /var/www/dashboard/admin/myDV-nano/*  /var/www/dashboard/admin/
 sudo rm -r /var/www/dashboard/admin/myDV-nano

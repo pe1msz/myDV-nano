2efb12caed599372cecba364f19080bae26dddb8
pe1msz <39623528+pe1msz@users.noreply.github.com>
2019-01-04 14:34:47 +0100

    webinterface
    update webinterface

--- run_me.sh
+++ run_me.sh
@@ -2,6 +2,7 @@
 sudo mount -o remount,rw /
 sudo git clone https://github.com/pe1msz/castbin /usr/bin/castbin
 sudo mv /usr/bin/castbin/* /usr/bin/. 
+sudo mv /usr/bin/CastFan /etc/init.d/.
 sudo rm -r /usr/bin/castbin
 sudo chmod +x /usr/bin/cast-avrdude
 sudo chmod +x /usr/bin/cast-reset
@@ -11,5 +12,9 @@ sudo mv /var/www/dashboard/admin/myDV-nano/*  /var/www/dashboard/admin/
 sudo rm -r /var/www/dashboard/admin/myDV-nano
 sudo chown -R www-data:www-data /var/www/dashboard/admin/fw
 sudo sudo chmod +x /var/www/dashboard/admin/fw/dvmega/flash_mega.sh
+sudo chmod +x /etc/init.d/CastFan
+cd /etc/init.d
+sudo update-rc.d CastFan defaults
 cd /var/www/dashboard/admin/fw/pyserial-3.2.1 
 sudo python setup.py install
+sudo reboot now
